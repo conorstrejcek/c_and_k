42e11c82630b2f3a9bfdcbc691a060607b928a62
OpenShift guest <theconorstrejcek@gmail.com>
2012-12-07 21:25:56 -0500

    cleaned up

--- conorstrejcek/config.php
+++ conorstrejcek/config.php
@@ -2,7 +2,7 @@
 require_once('./lib/Stripe.php');
 
 $stripe = array(
-    'secret_key'      => 'sk_test_IbvQIXfxgzKoLuygHSS76Nsj'),
+    'secret_key'      => 'sk_test_IbvQIXfxgzKoLuygHSS76Nsj',
     'publishable_key' => 'pk_test_pHml3AiM7SxyCrvGRcy8j68v'
 );
 
